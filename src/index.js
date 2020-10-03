0378de5b6eee6dbbdac8daeb576d7def06bbfe6b
TatsianaTarasova <70853063+TatsianaTarasova@users.noreply.github.com>
2020-10-03 12:58:31 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minNumber = array;
+  if(minNumber === undefined || array.length === 0) {
+    return 0;
+  } else
+  return Math.min.apply(null,array); 
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxNumber = array;
+  if(maxNumber === undefined || array.length === 0) {
+    return 0;
+  } else
+  return Math.max.apply(null,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgNunber = array;
+  if(avgNunber === undefined || array.length === 0) {
+    return 0;
+  } else 
+  var sum = 0;
+  array.forEach(function(value) {
+    sum += value;
+  });
+  var avg = sum/array.length;
+  return avg;
 }
